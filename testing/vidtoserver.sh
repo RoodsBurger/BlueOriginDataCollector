adbe8af1a18ffa1cb8325a7f9b71d860b6ca94c1
RoodsBurger <rodolfo.c.r1997@gmail.com>
2019-04-19 16:02:15 -0400

    added vid test script

---
+++ testing/vidtoserver.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DATE=$(date +"%Y-%m-%d_%H%M")
+
+raspivid -o $1-$DATE.h264 -t $2 -w 1920 -h 1080 -fps 30
+MP4Box -add $1-$DATE.h264 $1-$DATE.mp4
+rm $1-$DATE.h264
+spc $1-$DATE.mp4 $4@$3:~/Desktop
+rm $1-$DATE.mp4
